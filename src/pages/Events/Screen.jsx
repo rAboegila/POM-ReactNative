5d3bb939baa889825e995d1c8aec70c45989de71
MohamedOsama298 <mohamedosama135246@gmail.com>
2023-07-01 07:22:02 +0300

    added events filter static

--- src/pages/Events/Screen.jsx
+++ src/pages/Events/Screen.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, Box, Button, FlatList ,HStack,VStack} from "native-base";
 import DrawerIcon from "../../components/DrawerIcon/component";
-// import HomeButton from "../../components/HomeButton/component";
+import HomeButton from "../../components/HomeButton/component";
 
 import styles from "./styles";
 import EventCard from "../../components/EventCard/component";
@@ -11,7 +11,7 @@ export default function Events({ navigation }) {
       id: 1,
       name: "event",
       price: 100,
-      date: "5/10/2022",
+      date: "2023-7-10",
       type: "parkour",
       description:
         "loremipsumksladakdmklasdklsamdlkaslkdmklsamdlkmaskldmklamsdlkmsalkdmklasmdlkmaslkdmlksamdk",
@@ -20,7 +20,7 @@ export default function Events({ navigation }) {
       id: 2,
       name: "event",
       price: 100,
-      date: "5/10/2022",
+      date: "2023-7-3",
       type: "parkour",
       description:
         "loremipsumksladakdmklasdklsamdlkaslkdmklsamdlkmaskldmklamsdlkmsalkdmklasmdlkmaslkdmlksamdk",
@@ -29,7 +29,7 @@ export default function Events({ navigation }) {
       id: 3,
       name: "event",
       price: 100,
-      date: "5/10/2022",
+      date: "2023-8-10",
       type: "parkour",
       description:
         "loremipsumksladakdmklasdklsamdlkaslkdmklsamdlkmaskldmklamsdlkmsalkdmklasmdlkmaslkdmlksamdk",
@@ -38,16 +38,44 @@ export default function Events({ navigation }) {
       id: 4,
       name: "event",
       price: 100,
-      date: "5/10/2022",
+      date: "2025-8-10",
       type: "parkour",
       description:
         "loremipsumksladakdmklasdklsamdlkaslkdmklsamdlkmaskldmklamsdlkmsalkdmklasmdlkmaslkdmlksamdk",
     },
   ]; //
 
+  const [renderedEvents, setrenderedEvents]=useState(events);
+
   const routeToDetails = (event) => {
     navigation.navigate("Event Details", { event });
   };
+
+  const daysFromNow = (dateString) => {
+    const targetDate = new Date(dateString);
+    const today = new Date();
+    const diffTime = targetDate.getTime() - today.getTime();
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+  };
+
+  const filterEvents = (filter)=>{
+    let filteredEvents
+    switch (filter) {
+      case "Week":
+        filteredEvents =events.filter(event=>daysFromNow(event.date)<=7);
+        setrenderedEvents(filteredEvents);
+        break;
+      case "Month":
+        filteredEvents =events.filter(event=>daysFromNow(event.date)<=30);
+        setrenderedEvents(filteredEvents);
+        break;
+      default:
+        setrenderedEvents(events);
+        break;
+    }
+  }
+
   return (
     <>
     {/* <Box safeArea mb="3">
@@ -57,16 +85,20 @@ export default function Events({ navigation }) {
     </Box> */}
 
     <Box safeArea style={styles.container}>
+      <HStack mb="3" justifyContent="space-between" width="70%">
+        <Button style={styles.filterBtn} onPress={()=>filterEvents("All")}>All</Button>
+        <Button style={styles.filterBtn} onPress={()=>filterEvents("Week")}>In Week</Button>
+        <Button style={styles.filterBtn} onPress={()=>filterEvents("Month")}>In Month</Button>
+      </HStack>
         <FlatList  
         style={styles.list}
-        contentContainerStyle={{flexGrow:1}}
-        data={events}
+        data={renderedEvents}
         renderItem={({item})=><EventCard event={item} routeToDetails={routeToDetails}/>}
         keyExtractor={item=>item.id}
         />
         {/* {events.map((event)=><EventCard event={event} routeToDetails={routeToDetails}/>)} */}
       </Box>
-    {/* <HomeButton navigation={navigation} /> */}
+    <HomeButton navigation={navigation} />
   </>
   );
 }
--- src/pages/Events/styles.js
+++ src/pages/Events/styles.js
@@ -2,12 +2,19 @@ import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
   container: {
+    flex:1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
     padding:10
   },
   list:{
-    width:"90%"
+    width:"100%",
+    flexGrow:1
+  },
+  filterBtn:{
+    backgroundColor:"#14ae5c",
+    color:"white",
+    borderRadius:5
   }
 });
