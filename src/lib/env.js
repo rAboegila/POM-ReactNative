8760c1be684587415fd67d0b88d397a3251c10ec
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-07-04 12:45:23 +0300

    env variables

---
+++ src/lib/env.js
@@ -0,0 +1,4 @@
+export const LOCAL_BASE_URL = "http://192.168.1.19:5000/pom/";
+export const DEPLOY_BASE_URL = "https://powerofmotionapi.onrender.com/pom/";
+export const PROFILE_IMAGE_URL =
+  "https://powerofmotionapi.onrender.com/uploads/user/profiles/";
