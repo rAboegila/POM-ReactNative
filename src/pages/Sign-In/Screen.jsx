44d68d56d1b927dd24c2484105cc8d7a641c67ba
rAboegila <rawan.moyasser.aboegila@gmail.com>
2023-07-05 06:21:47 +0300

    sign In auto correct disabled

--- src/pages/Sign-In/Screen.jsx
+++ src/pages/Sign-In/Screen.jsx
@@ -86,6 +86,7 @@ export default function SignIn({ navigation }) {
             <FormControl.Label>Email</FormControl.Label>
             <Input
               value={email}
+              autoCorrect={false}
               onChangeText={(value) => {
                 setEmail(value);
               }}
@@ -99,6 +100,7 @@ export default function SignIn({ navigation }) {
             <Input
               value={password}
               type={show ? "text" : "password"}
+              autoCorrect={false}
               InputRightElement={
                 <Pressable onPress={() => setShow(!show)}>
                   <Icon
